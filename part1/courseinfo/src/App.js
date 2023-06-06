f989d32e6d95f69bafe54afee1c049a2cd699265
Lachlan Young <lachie.s.young@gmail.com>
2023-06-06 12:44:53 +1000

    exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -29,24 +29,26 @@ const Total = ({ number }) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={{'part1': part1, 'part2': part2, 'part3': part3}}/>
-      <Total number={part1.exercises+part2.exercises+part3.exercises} />
+      <Content parts={{'part1': parts[0], 'part2': parts[1], 'part3': parts[2]}}/>
+      <Total number={parts[0].exercises+parts[1].exercises+parts[2].exercises} />
     </div>
   )
 }
