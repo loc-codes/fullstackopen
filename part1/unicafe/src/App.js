cdf2df6c56559a52fb04098e7af7477295fb06ac
Lachlan Young <lachie.s.young@gmail.com>
2023-06-06 13:05:09 +1000

    exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Title = ({ text }) => {
   return (
-    <div className="App">
-      <h1>hey mum</h1>
-    </div>
-  );
+    <h1>{text}</h1>
+  )
 }
 
-export default App;
+
+const Button = ({ name, handleClick }) => {
+  return (
+    <button onClick={handleClick}>{name}</button>
+  )
+}
+
+const Statistic = ({ name, number }) => {
+  return (
+    <div>{name} {number}</div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => {
+    setGood(good + 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Title text='give feedback'/>
+      <Button name='good' handleClick={goodClick} />
+      <Button name='neutral' handleClick={neutralClick} />
+      <Button name='bad' handleClick={badClick} />
+      <Title text='statistics' />
+      <Statistic name='good' number={good} />
+      <Statistic name='neutral' number={neutral} />
+      <Statistic name='bad' number={bad} />
+    </div>
+  )
+}
+
+export default App
