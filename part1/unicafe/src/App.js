24f09f4b5f9616ca9752cdc0bce81de56d15c596
Lachlan Young <lachie.s.young@gmail.com>
2023-06-06 14:09:40 +1000

    exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,7 +17,7 @@ const Statistic = ({ name, number }) => {
   return (
     <div>{name} {number}</div>
   )
-} //already defined from previous exercise
+} 
 
 const App = () => {
   // save clicks of each button to its own state
@@ -40,7 +40,7 @@ const App = () => {
 
   const total = good+neutral+bad
   let positive = good/total
-
+  const initialised = !(good===0&&neutral===0&&bad===0)
   return (
     <div>
       <Title text='give feedback'/>
@@ -48,12 +48,17 @@ const App = () => {
       <Button name='neutral' handleClick={neutralClick} />
       <Button name='bad' handleClick={badClick} />
       <Title text='statistics' />
+      {/* Add ternary operator for DRY conditional rendering*/}
+      {initialised ?                  
+      (<>
       <Statistic name='good' number={good} />
       <Statistic name='neutral' number={neutral} />
       <Statistic name='bad' number={bad} />
       <Statistic name='all' number={total} />
       <Statistic name='average' number={(good*1+bad*-1)/total} />
       <Statistic name='positive' number={`${positive*100}%`} />
+      </>) 
+      : (<div>No feedback given</div>)}
     </div>
   )
 }
