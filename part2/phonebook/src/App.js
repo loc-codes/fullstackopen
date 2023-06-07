7daab4d3741123840a80cf4007b03335c60a648d
Lachlan Young <lachie.s.young@gmail.com>
2023-06-07 23:07:53 +1000

    exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,10 +6,16 @@ const Contact = ( {name, number } ) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [active, setActive] = useState(false)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -21,18 +27,31 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const newObj = {name: newName, number: newNumber}
+    const newObj = {name: newName, number: newNumber, id: persons.length+1}
     persons.every((object) => 
-    JSON.stringify(object.number) !== JSON.stringify(newObj.number))
+    JSON.stringify(object.name) !== JSON.stringify(newObj.name))
     ? setPersons(() => persons.concat(newObj))
-    : alert(`${newNumber} is already added to phonebook!`)
+    : alert(`${newName} is already added to phonebook!`)
     setNewName('')
     setNewNumber('')
   }
 
+  const handleFilter = (event) => {
+    setFilter(event.target.value.toLowerCase())
+  }
+
+  const filteredPersons = (filter === '')
+    ? persons
+    : persons.filter(person => 
+      person.name.toLowerCase().includes(filter))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter show with <input onChange={handleFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -43,9 +62,9 @@ const App = () => {
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>{
+      {filteredPersons.map(person => {
         return (
-        <Contact key={person.name} name={person.name} number={person.number}/>
+        <Contact key={person.id} name={person.name} number={person.number}/>
         )
       })}
     </div>
