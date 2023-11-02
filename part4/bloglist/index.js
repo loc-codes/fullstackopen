5ff15f5cf8902feb80e1fab6ec61998778e41ecd
Lachlan Young <lachie.s.young@gmail.com>
2023-11-03 07:43:52 +1100

    Question 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,36 +1,7 @@
-const express = require('express')
-  const app = express()
-  const cors = require('cors')
-  require('dotenv').config()
-  const mongoose = require('mongoose')
-  const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  const Blog = mongoose.model('Blog', blogSchema)
-  MONGODB_URI = process.env.MONGODB_URI
-  mongoose.connect(MONGODB_URI)
-  app.use(cors())
-  app.use(express.json())
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-  app.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog .save()
-      .then(result => {
-        response.status(201).json(result)
-    }) 
-    })
-  const PORT = 3003
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
