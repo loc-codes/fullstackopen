be7792951da770b061731e45925af21edf92f7b8
Lachlan Young <lachie.s.young@gmail.com>
2023-11-04 14:23:18 +1100

    Question 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const ld = require('lodash')
+
 const dummy = () => {
     return 1
 }
@@ -14,8 +16,20 @@ const favouriteBlog = (blogs) => {
     return { title, author, likes }
 }
 
+const mostBlogs = (blogs) => {
+    const groupedBlogs = ld.groupBy(blogs, 'author')
+    const authorCounts = ld.map(groupedBlogs, (blogs, author) => {
+        return {
+            author: author,
+            blogs: blogs.length
+        }
+    })
+    return ld.orderBy(authorCounts, 'blogs', 'desc')[0]
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs
 }
