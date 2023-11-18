a6e682d45a5d36160d4cf9c86809c3859345928c
Lachlan Young <lachie.s.young@gmail.com>
2023-11-18 11:49:40 +1100

    Part 5.2 examples

--- part5/notes-frontend/src/App.jsx
+++ part5/notes-frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
 import Footer from './components/Footer'
+import Togglable from './components/Toggable'
+import NoteForm from './components/NoteForm'
 import noteService from './services/notes'
 import loginService from './services/login'
-import login from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
+  const [user, setUser] = useState(null) 
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null) 
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
@@ -30,55 +29,16 @@ const App = () => {
       .then(initialNotes => {
         setNotes(initialNotes)
       })
-  }, [])
+  }, [])  
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-          /> 
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-          /> 
-        </div>
-      <button type="submit">login</button>
-    </form>
-  )
+  const noteFormRef = useRef()
 
-
-  const noteForm = () => (
-    <form onSubmit={addNote}>
-      <input
-        value={newNote}
-        onChange={handleNoteChange}
-      />
-      <button type="submit">save</button>
-    </form>
-  )
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
-    }
-  
+  const addNote = (noteObject) => { 
+    noteFormRef.current.toggleVisibility()   
     noteService
       .create(noteObject)
         .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
-        setNewNote('')
       })
   }
 
@@ -101,13 +61,9 @@ const App = () => {
       })
   }
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
-  }
-  
-  const handleLogin = async (event) => {
-    event.preventDefault()
 
+  
+  const handleLogin = async ({ username, password }) => {
     try {
       const user = await loginService.login(
         { username, password }
@@ -119,8 +75,6 @@ const App = () => {
 
       noteService.setToken(user.token)
       setUser(user)
-      setUsername('')
-      setPassword('')
     }
     catch (exception) {
       setErrorMessage('Wrong credients')
@@ -132,6 +86,22 @@ const App = () => {
     console.log('logging in with', username, password)
   }
 
+  const loginForm = () => (
+    <Togglable buttonLabel='login'>
+      <LoginForm
+        handleLogin={handleLogin}
+      />
+    </Togglable>
+  )
+
+  const noteForm = () => (
+    <Togglable buttonLabel="new note" ref={noteFormRef}>
+      <NoteForm
+        createNote={addNote}
+      />
+    </Togglable>
+  )
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
@@ -139,17 +109,15 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
-
       <Notification message={errorMessage} />
-
-      {!user && loginForm()}
+      {!user && loginForm()} 
       {user && <div>
         <p>{user.name} logged in</p>
-        {noteForm()}
-      </div>}
+          {noteForm()}
+        </div>
+      }
 
       <h2>Notes</h2>
-
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
---
+++ part5/notes-frontend/src/components/NoteForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const NoteForm = ({ createNote }) => {
+    const [newNote, setNewNote] = useState('')
+
+    const addNote = (event) => {
+        event.preventDefault()
+        createNote({
+          content: newNote,
+          important: true
+        })
+        setNewNote('')
+      }
+
+    return (
+        <form onSubmit={addNote}>
+        <input
+            value={newNote}
+            onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">save</button>
+        </form>
+    )
+}
+
+export default NoteForm
