866f3efa445ffe7c941a2aacd248e654acc30665
Lachlan Young <lachie.s.young@gmail.com>
2023-11-25 18:38:23 +1100

    6.2 notes

--- part6/notes-redux/src/App.jsx
+++ part6/notes-redux/src/App.jsx
@@ -1,10 +1,17 @@
 import NewNote from './Components/NewNote'
 import Notes from './Components/Notes'
+import VisibilityFilter from './Components/VisibilityFilter'
+
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
+
   return(
     <div> 
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
