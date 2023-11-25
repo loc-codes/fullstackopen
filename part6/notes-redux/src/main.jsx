c2aff80a8dc6041028b8ff90f1cb0901c1c4b9c4
Lachlan Young <lachie.s.young@gmail.com>
2023-11-25 16:08:39 +1100

    notes part 6.1

--- part6/notes-redux/src/main.jsx
+++ part6/notes-redux/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+  import ReactDOM from 'react-dom/client'
+  import { createStore } from 'redux'
+  import { Provider } from 'react-redux'
+  import App from './App'
+  import noteReducer from './reducers/noteReducer'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  const store = createStore(noteReducer)
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
 )
--- part6/notes-redux/src/reducers/noteReducer.js
+++ part6/notes-redux/src/reducers/noteReducer.js
@@ -9,10 +9,29 @@ const noteReducer = (state = [], action) => {
                 ...noteToChange,
                 important: !noteToChange.important
             }
-            return state.map(note => (note.id === id ? note : changedNote))
+            return state.map(note => (note.id === id ? changedNote : note))
         default: 
             return state
     }
   }
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+    return {
+        type: 'NEW_NOTE',
+        payload: {
+        content,
+        important: false,
+        id: generateId()
+        }
+    }
+}
+export const toggleImportanceOf = (id) => {
+    return {
+        type: 'TOGGLE_IMPORTANCE',
+        payload: { id }
+    }
+}
 
 export default noteReducer
